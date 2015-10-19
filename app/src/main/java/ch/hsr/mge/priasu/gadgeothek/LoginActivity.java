b6b127dfb9a37297ddafe4560be47325f6b50e5c
Sperma <spam.me.ass@outlook.com>
2015-10-19 13:56:44 +0200

    hg

--- app/src/main/java/ch/hsr/mge/priasu/gadgeothek/LoginActivity.java
+++ app/src/main/java/ch/hsr/mge/priasu/gadgeothek/LoginActivity.java
@@ -320,4 +320,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mEmailView.setAdapter(adapter);
     }
+
+    private void registerActivity(){
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
 }
